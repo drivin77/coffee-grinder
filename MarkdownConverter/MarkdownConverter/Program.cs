92338d8a0640d175bbd4fd7361ba2a468451aeef
Daniel Rivin (SolanaBeach) <daniel.rivin@disney.com>
2015-03-30 16:19:23 -0700

    Updates to Markdown Converter.  Some progress, but something doesn't feel right.

--- MarkdownConverter/MarkdownConverter/Program.cs
+++ MarkdownConverter/MarkdownConverter/Program.cs
@@ -1,18 +1,40 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics.Eventing.Reader;
 using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace MarkdownConverter
 {
     static class Program
     {
-        private static Hashtable _symbolHash;
+        private static Regex _emRegex;
+        private static Regex _em2Regex;
+        private static Regex _strongRegex;
+        private static Regex _strong2Regex;
+        private static Regex _ampRegex;
+        private static Regex _ltRegex;
+
+        private static Dictionary<String, HtmlTag> _symbolHash;
+
+        private static HtmlTag _paragraphTag;
 
         static void Main(string[] args)
         {
+            _emRegex = new Regex("(\\*\\w+|\\w+\\*)");
+            _em2Regex = new Regex("(_\\w+|\\w+_)");
+            _strongRegex = new Regex("(\\*\\*\\w+|\\w+\\*\\*)");
+            _strong2Regex = new Regex("(__\\w+|\\w+__)");
+            _ampRegex = new Regex("");
+            _ltRegex = new Regex("\\w*<\\s+");
+
             var emTag = new HtmlTag("<em>", "</em>");
             var strongTag = new HtmlTag("<strong>", "</strong>");
-            _symbolHash = new Hashtable
+            _paragraphTag = new HtmlTag("<p>", "</p>");
+            _symbolHash = new Dictionary<String, HtmlTag>()
             {
                 {"#",       new HtmlTag("<h1>", "</h1>")},
                 {"##",      new HtmlTag("<h2>", "</h2>")},
@@ -29,20 +51,52 @@ namespace MarkdownConverter
             try
             {
                 var markupFile = new StreamReader("Markdown.txt");
-                var htmlFile = new FileStream("htmlConversion.html", FileMode.Create);
+                var htmlFile = new StreamWriter("htmlConversion.html", false);
                 while (markupFile.Peek() >= 0)
                 {
-                    var line = markupFile.ReadLine();
+                    var line = markupFile.ReadToEnd();
 
-                    var tokens = line.Split(new[] {' '}, StringSplitOptions.None);
+                    // split file into paragraph tokens
+                    var tokens = Regex.Split(line, "\r\n\r\n");
 
                     foreach (var token in tokens)
                     {
+                        // if paragraph doesn't start with #, or a list symbol, then it's a paragraph
+                        switch (token[0])
+                        {
+                            // header case.  Must have a space after the header symbol.
+                            case '#':
+                                if (Regex.IsMatch(token, "#+\\s"))
+                                    CreateHeader(token, htmlFile);
+                                break;
+                            
+                            // unordered list case.  Must have a space after the list symbol.
+                            case '-':
+                            case '+':
+                            case '*':
+                                if (Regex.IsMatch(token, "(-|\\+|\\*)\\s"))
+                                    CreateUnorderedList(token, htmlFile);
+                                goto default;
+
+                            default:
+                                // we can't have a regex in a case statement, so the ordered
+                                // list has to go in default as the ordered list can specify any
+                                // number folowed by a period and a space.
+                                if (Regex.IsMatch(token, "\\d+\\.\\s"))
+                                    CreateOrderedList(token, htmlFile);
+
+                                else
+                                    CreateParagraph(token, htmlFile);
+
+                                break;
+                        }
                         Console.WriteLine(token);
                     }
                     Console.WriteLine();
                     
                 }
+
+                htmlFile.Close();
 
                 Console.ReadKey();
             }
@@ -52,6 +106,119 @@ namespace MarkdownConverter
                 throw;
             }
             
+        }
+
+        private static void CreateParagraph(string token, StreamWriter htmlFile)
+        {
+            htmlFile.Write(_paragraphTag.OpenTag);
+            ParseAndWriteInlines(token, htmlFile);
+            htmlFile.Write(_paragraphTag.CloseTag);
+        }
+
+        private static void CreateOrderedList(string token, StreamWriter htmlFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreateUnorderedList(string token, StreamWriter htmlFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Header is a simple case - 1-6 '#' symbols followed by text, followed by any number
+        /// of '#' symbols which we ignore.
+        /// </summary>
+        /// <param name="token">the token to parse</param>
+        /// <param name="htmlFile">the file to write to</param>
+        private static void CreateHeader(string token, StreamWriter htmlFile)
+        {
+            var headerMatch = Regex.Match(token, "(#+)\\s(.*)\\s(#*)$", RegexOptions.Multiline);
+            if (! headerMatch.Success)
+                throw new ArgumentException(
+                    String.Format(
+                        "Header token not formatted properly: {0}.",
+                        token
+                    )
+                );
+
+            var headerString = headerMatch.Groups[1].Value;
+            var headerHtmlTags = _symbolHash[headerString];
+            
+            if (headerHtmlTags == null)
+                throw new ArgumentException(
+                    String.Format(
+                        "Header token not formatted properly: {0}.",
+                        token
+                    )
+                );
+
+            htmlFile.Write(headerHtmlTags.OpenTag);
+            ParseAndWriteInlines(headerMatch.Groups[2].Value, htmlFile);
+            htmlFile.Write(headerHtmlTags.CloseTag);
+        }
+
+        /// <summary>
+        /// Inlines are emphasis (* or _) strong (** or __), or ampersand (&) and less than (&lt;)
+        /// escaping.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="htmlFile"></param>
+        private static void ParseAndWriteInlines(string token, StreamWriter htmlFile)
+        {
+            // keep closing tags on a stack for emphasis and strong for nesting.
+            var htmlTagStack = new Stack<string>();
+
+            // line to write to output file
+            var fileWriteLine = new StringBuilder();
+
+            // iterate through every token (separated by whitespace in the line and transform 
+            // in-lines in place
+            var wordTokens = token.Split(' ');
+            foreach (var wordToken in wordTokens)
+            {
+                // em case
+                if (Regex.IsMatch(token, "^(\\*|_)\\w+"))
+                {
+                    wordToken.
+                }
+            }
+            /*
+            LastCharSeen lastChar;
+
+            var tokenLength = token.Length;
+            for (var i = 0; i < tokenLength; ++i)
+            {
+                switch (token[i])
+                {
+                    case '*':
+                    case '_':
+                        var isStrong = false;
+                        var isEm = false;
+                        // check for valid <em> or <strong>
+                        if (i < tokenLength - 1)
+                        {
+                            // <strong> case (potentially)
+                            if (token[i].Equals(token[i + 1]))
+                            {
+                                if (i < tokenLength - 2 && ! token[i + 2].Equals(' '))
+                                {
+                                    ++i;
+                                    isStrong = true;
+                                }
+                            }
+
+                            // <em> case
+                            else if (!token[i + 1].Equals(' '))
+                            {
+                                isEm = true;
+                            }
+                        }
+
+                    default:
+
+                }*/
+            }
         }
     }
 }
